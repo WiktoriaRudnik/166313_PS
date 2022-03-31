496014473d77f5cc2c5cf10a0293c8478ffc5e5b
WiktoriaRudnik <97965118+WiktoriaRudnik@users.noreply.github.com>
2022-03-31 19:37:03 +0200

    Add files via upload

---
+++ zad6/za2/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+    const int N =100;
+    short tab[N];
+    printf("Podaj liczbe elementow: ");
+    int n;
+    scanf("%d",&n);
+    if(n<=0||n>N){
+        printf("Liczba elementow musi byæ z przedzia³u [1, %d\n]!", N);
+        return 1;
+    }
+short lim=999;
+srand(time(0));
+for (short*p=tab; p<tab+n;++p){
+    *p = rand()%lim+1;
+}
+for (short*p=tab; p<tab+n;++p){
+    printf("%5d",*p);
+}
+printf("\n");
+for (short*p=tab;p<tab+n;++p){
+    if(*p<=0){
+        continue;
+    }
+    if(*p%2==1){
+            *p=2 * *p;
+    }else *p=-*p;
+}
+printf("\n");
+
+for (short*p=tab; p<tab+n;++p)
+printf("%5d",*p);
+for (short*p=tab;p<tab+n;++p){
+    if(*p>0){
+            *p=1;
+    }else *p=-1;
+}
+printf("\n");
+for (short*p=tab; p<tab+n;++p)
+printf("%5d",*p);
+return 0;
+}
---
+++ zad6/za3/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    const int N=2;
+    short tab[N];
+    printf("Podaj liczby do tablicy \n");
+    scanf("%d %d",&a,&b);
+    printf("%d %d",a,b);
+    return 0;
+}
